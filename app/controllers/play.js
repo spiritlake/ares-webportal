4e142d6b051ceed9d07e0fa174f49d741c51221c
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2023-04-17 14:01:55 -0400

    mute convos from sidebar

--- app/controllers/play.js
+++ app/controllers/play.js
@@ -12,10 +12,10 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
     gameSocket: service(),
     session: service(),
     favicon: service(),
-  
+
     // Scenes
     currentScene: null,
-  
+
     // Chat
     selectedChannel: null,
     showNewConversation: false,
@@ -25,7 +25,8 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
     showAllPms: false,
     poseChar: null,
     newPage: null,
-      
+    hidePage: null,
+
     // Both
     scrollPaused: false,
 
@@ -33,7 +34,7 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
       this._super(...arguments);
       this.set('newConversationList', []);
     },
-  
+
     sortedChannels: computed('model.chat.channels.@each.title', function() {
       return this.get('model.chat.channels').filter(c => !c.is_page).sort((a, b) => a.title.localeCompare(b.title));
     }),
@@ -41,22 +42,22 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
     sortedPageThreads: computed('model.chat.channels.@each.title', function() {
       return this.get('model.chat.channels').filter(c => c.is_page).sort((a, b) => a.title.localeCompare(b.title));
     }),
-  
+
     emptyPrompt: computed('selectedChannel', 'currentScene', function() {
       return !this.selectedChannel && !this.currentScene;
     }),
-    
+
     onSceneActivity: function(type, msg, timestamp ) {
       let splitMsg = msg.split('|');
       let sceneId = splitMsg[0];
       let char = splitMsg[1];
       let notify = true;
       let currentUsername = this.get('currentUser.name');
-      
+
         // For poses we can just add it to the display.  Other events require a reload.
         if (sceneId === this.get('currentScene.id')) {
           let scene = this.currentScene;
-          
+
           notify = this.updateSceneData(scene, msg, timestamp);
 
           if (currentUsername != char) {
@@ -65,11 +66,11 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
           else {
             notify = false;
           }
-          
+
           if (notify) {
             this.gameSocket.notify(`New activity from ${char} in scene ${sceneId}.`);
             this.scrollWindow();
-          }          
+          }
         }
         else {
             this.get('model.scenes').forEach(s => {
@@ -80,11 +81,11 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
                     this.gameSocket.notify(`New activity from ${char} in one of your other scenes (${sceneId}).`);
                   }
                 }
-            });            
+            });
         }
         this.markSceneRead(sceneId);
     },
-    
+
     onChatMessage: function(type, msg, timestamp) {
       let msgData = JSON.parse(msg);
       let channelKey = msgData.key;
@@ -92,7 +93,7 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
       let newMessage = msgData.message;
       let author = msgData.author;
       let localTimestamp = localTime(timestamp);
-      
+
       let channel = this.getChannel(channelKey);
       if (!channel) {
         channel = this.addPageChannel(msgData);
@@ -121,7 +122,7 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
           }
       }
     },
-    
+
     onJoinedScene: function(type, msg, timestamp ) {
       let sceneData = EmberObject.create(JSON.parse(msg));
 
@@ -129,7 +130,7 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
         this.model.scenes.pushObject(sceneData);
       }
     },
-    
+
     resetOnExit: function() {
       this.set('currentScene', null);
       this.set('selectedChannel', null);
@@ -139,12 +140,12 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
       this.set('showAddChannel', false);
       this.set('scrollPaused', false);
     },
-    
+
     setupController: function(model) {
       this.setupCallback();
       this.set('poseChar', model.chat.pose_chars[0]);
     },
-    
+
     setupCallback: function() {
         let self = this;
         this.gameSocket.setupCallback('new_scene_activity', function(type, msg, timestamp) {
@@ -156,52 +157,52 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
         this.gameSocket.setupCallback('joined_scene', function(type, msg, timestamp) {
             self.onJoinedScene(type, msg, timestamp) } );
     },
-    
+
     scrollWindow: function() {
       // Unless scrolling paused or edit active.
       let poseEditActive =  this.currentScene && this.get('currentScene.poses').some(p => p.editActive);
       if (this.scrollPaused || poseEditActive) {
         return;
       }
-      
+
       try {
         let chatWindow = $('#chat-window')[0];
         if (chatWindow) {
             $('#chat-window').stop().animate({
                 scrollTop: chatWindow.scrollHeight
-            }, 400);    
-        }  
-        
+            }, 400);
+        }
+
         let sceneWindow = $('#live-scene-log')[0];
         if (sceneWindow) {
           $('#live-scene-log').stop().animate({
               scrollTop: $('#live-scene-log')[0].scrollHeight
-          }, 400);           
+          }, 400);
         }
       }
       catch(error) {
         // This happens sometimes when transitioning away from screen.
-      }   
+      }
     },
-    
+
     channelsByActivity: computed('model.chat.channels.@each.last_activity', function() {
        return this.get('model.chat.channels').sort(function(a,b){
         return new Date(b.last_activity) - new Date(a.last_activity);
        });
     }),
-    
+
     anyNewActivity: computed('model.chat.channels.@each.{is_unread,new_messages}', 'model.scenes.@each.is_unread', function() {
       return this.get('model.chat.channels').any(c => c.is_unread || c.new_messages > 0) || this.get('model.scenes').any(s => s.is_unread );
     }),
-        
-    
+
+
     addPageChannel: function(data) {
-      let channel = { title: data.title, 
-        key: data.key, 
-        enabled: true, 
-        can_join: true, 
+      let channel = { title: data.title,
+        key: data.key,
+        enabled: true,
+        can_join: true,
         can_talk: true,
-        is_page: true, 
+        is_page: true,
         muted: false,
         poseable_chars: data.poseable_chars,
         messages: [],
@@ -210,15 +211,15 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
       this.get('model.chat.channels').pushObject(channel);
       return channel;
     },
-    
+
     getChannel: function(channelKey) {
       return this.get('model.chat.channels').find(c => c.key === channelKey);
     },
-    
+
     markSceneRead: function(sceneId) {
      this.gameApi.requestOne('markSceneRead', { id: sceneId }, null);
     },
-    
+
     markPageThreadRead: function(threadId) {
       let api = this.gameApi;
       api.requestOne('markPageThreadRead', { thread_id: threadId }, null)
@@ -226,9 +227,9 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
           if (response.error) {
               return;
           }
-      }); 
+      });
     },
-    
+
     changeChannel: function(channel) {
       this.set('selectedChannel', channel);
       this.set('currentScene', null);
@@ -236,12 +237,12 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
       set(channel, 'is_unread', false);
       if (this.get('selectedChannel.is_page'))  {
         this.markPageThreadRead(channel.key);
-      } 
-      
+      }
+
       let self = this;
       setTimeout(() => self.scrollWindow(), 150, self);
     },
-    
+
     switchScene: function(scene) {
       scene.set('is_unread', false);
       this.set('currentScene', scene);
@@ -250,13 +251,13 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
       setTimeout(() => self.scrollWindow(), 150, self);
       this.markSceneRead(scene.id);
     },
-    
-    actions: {        
-            
+
+    actions: {
+
       joinChannel: function(channelName) {
           let api = this.gameApi;
           this.set('showAddChannel', false);
-                    
+
           api.requestOne('joinChannel', { channel: channelName, char: this.poseChar.name }, null)
           .then( (response) => {
               if (response.error) {
@@ -269,24 +270,24 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
               this.changeChannel(data);
           });
       },
-    
-      
+
+
         refresh() {
             this.resetOnExit();
             this.send('reloadModel');
         },
-        
+
         setScroll(option) {
           this.set('scrollPaused', !option);
           if (option) {
             this.scrollWindow();
           }
         },
-        
+
         scrollDown() {
           this.scrollWindow();
         },
-        
+
         switchScene(id) {
           let api = this.gameApi;
           var scene;
@@ -295,7 +296,7 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
                 scene = s;
               }
           });
-          
+
           if (scene.lazy_loaded === true) {
             api.requestOne('liveScene', { id: scene.id }, null)
             .then( (response) => {
@@ -310,10 +311,10 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
             this.switchScene(scene);
           }
         },
-        
+
         changeChannel: function(channel) {
           let api = this.gameApi;
-          
+
           if (channel.lazy_loaded === true) {
             api.requestOne('loadChatMessages', { key: channel.key, is_page: channel.is_page }, null)
             .then( (response) => {
@@ -327,18 +328,36 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
           } else {
             this.changeChannel(channel);
           }
-          
+
         },
-        
+
+        hidePage: function(hidden) {
+          let channelKey = this.get('selectedChannel.key');
+          this.gameApi.requestOne('hidePageThread', { thread_id: channelKey, hidden: hidden }, null)
+          .then( (response) => {
+              if (response.error) {
+                  return;
+              }
+              this.set('channel.is_hidden', hidden);
+              this.set('channel.is_recent', !hidden);
+              if (hidden) {
+                this.flashMessages.success("Conversation will no longer appear on the recent list.");
+              } else {
+                this.flashMessages.success("Conversation will appear again on the recent list.");
+              }
+
+            });
+        },
+
         conversationListChanged(newList) {
             this.set('newConversationList', newList);
         },
-        
+
         startConversation: function() {
           let api = this.gameApi;
           let message = this.newPage;
           let names = (this.newConversationList || []).map(p => p.name);
-          
+
           if (names.length === 0) {
             this.flashMessages.danger("You haven't entered any recipients.");
             return;
@@ -350,7 +369,7 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
           if (!this.poseChar) {
             this.flashMessages.danger("You hven't selected a charcter.");
           }
-          
+
           this.set(`newPage`, '');
           this.set('selectedChannel', null);
           this.set('showNewConversation', false);
@@ -364,8 +383,8 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
               let channel = this.getChannel(response.thread.key);
               if (!channel) {
                 channel = response.thread;
-                this.get('model.chat.channels').pushObject(channel);  
-              } 
+                this.get('model.chat.channels').pushObject(channel);
+              }
               this.changeChannel(channel);
           });
         },
@@ -373,12 +392,12 @@ export default Controller.extend(AuthenticatedController, SceneUpdate, {
         pauseScroll() {
           this.set('scrollPaused', true);
         },
-        
+
         unpauseScroll() {
           this.set('scrollPaused', false);
           this.scrollWindow();
         },
-        
+
         poseCharChanged(char) {
           this.set('poseChar', char);
         }
